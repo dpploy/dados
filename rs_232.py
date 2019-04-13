72206127cc76c2ff6bce2e4c5fa0380b31846ca7
seamuss1 <seamusdgallagher@gmail.com>
2019-04-13 11:36:21 -0900

    Moved working code over to rs_232, dados.py now calls rs_232.create_instance()

--- rs_232.py
+++ rs_232.py
@@ -89,9 +89,96 @@ class RS_232():
 
         return
 
+    def create_instance(self,*args,**kwargs):
+        #Creates a new thread, then kills main thread
+        worker = threading.Thread(target=self.__start_com, name='start_com')
+        worker.start()
+        return
 #*********************************************************************************
 # Private helper functions (internal use: __)
 #*********************************************************************************
+    def __start_com(self):
+        self.socket_list = []
+        self.init_port=60000
+        self.port=60001
+        self.host=socket.gethostbyname(socket.gethostname())
+        print('Host IP-address: {}'.format(self.host()))
+        #initialize serial object, parameters hard-coded for now
+        ser=serial.Serial(port='/dev/ttyUSB0',baudrate=9600,timeout=5,
+                          stopbits = serial.STOPBITS_ONE,
+                          parity = serial.PARITY_NONE,
+                          bytesize=serial.EIGHTBITS)
+        socket_worker=threading.Thread(target=self.__initialize_socket,name='init_worker')
+        socket_worker.start()
+        print('COMM START')
+        olddata=''
+        while True:
+            #Send request string, specific to IR7040
+            ser.write('\r\nP0001 1289Od 7F}'.encode('ascii'))
+            original_line=ser.readline()
+            line = str(original_line.decode('utf-8', errors='replace').strip())
+            if line == olddata:
+                time.sleep(.25)
+                continue
+            self.timestamp=str(datetime.datetime.now())[:-7]
+            olddata=line
+            splitline=line.split()
+            splitline.append(self.timestamp)
+            print(line)
+            print(splitline)
+            for n in range(2,8):
+                splitline[n] = splitline[n][0]+'.'+splitline[n][1:3]+'e'+splitline[n][3:]
+            filename='data/Stack_monitor_data_{}.csv'.format(str(datetime.datetime.now())[:10])
+            if not os.path.isfile(filename):
+                with open(filename,'a') as f:
+                    f.write('Type, Callback, Ch1_rate_filtered, Ch1_rate_unfiltered, Ch2_rate_filtered, Ch2_rate_unfiltered, Ch4_rate_filtered, Ch4_rate_unfiltered, Checksum, Date and Time\n')
+            padding = ', '.join(splitline)
+            commaline=padding
+            padding += (" " * (100 - len(padding)))
+            c=0
+            for conn in self.socket_list:
+                try:
+                    conn.send(padding.encode())
+                except Exception as e:
+                    print(traceback.print_exc())
+                    conn.close()
+                    del self.socket_list[c]
+                c+=1
 
+            if 'init_woker' not in [f.name for f in threading.enumerate()]:
+                socket_worker=threading.Thread(target=self.__initialize_socket,name='init_worker')
+                socket_worker.start()
+            with open(filename,'a') as f:
+                f.write('{}\n'.format(commaline))
+        self.conn.close()
 
+    def __initialize_socket(self):
+        try:
+            print('initial socket')
+            #Initial Socket for establishing permanent Port number
+            sock=socket.socket()
+            sock.settimeout(60)
+            sock.bind((self.host,self.init_port))
+            sock.listen(5)
+            conn,addr=sock.accept()
+            self.port+=1
+            string='port, {}, host, {}'.format(self.port,self.host)
+            string+=' '*(40-len(string))
+            conn.send(string.encode())
+            time.sleep(0.5)
+            print('Socket Initialized!')
+            conn.close()
+            #Reestablishes connection with new port
+            s = socket.socket()
+            s.settimeout(60)
+            print(self.port)
+            s.bind((self.host, self.port))
+            s.listen(5)
+            conn, addr = s.accept()
+            #new socket connections appended to list
+            self.socket_list.append(conn)
+            print('Connected to:{},{}'.format(conn,addr))
+        except Exception as e:
+            return        
+        return
 #======================= end class Dados: ========================================
