3a2ebdc1ea9bf7c05a54fa7faac5f4ef2f4d52aa
Seamus Gallagher <seamusdgallagher@gmail.com>
2019-06-14 17:50:22 -0400

    Improvements to stability, added matplotlib toolbar functionality
    Switched real time plotting method from deleting and drawing lines on the ax, to updating the data on the same line. This fixed a lot of the bugs.
    Also included warning box to stop users from navigating out of the recorded dates

--- Parse-Database/tkinter_data_parser.py
+++ Parse-Database/tkinter_data_parser.py
@@ -13,7 +13,9 @@ class App(tk.Frame):
         for f in ['input','output']:
             if not os.path.isdir(f):
                 os.makedirs(f)
-        self.start()
+        self.plot_chan=tk.StringVar()
+        self.plot_chan.set('1')
+        x,y = self.start()
         frame1=tk.Frame(master=parent)
         frame1.pack()
         frame2=tk.Frame(master=parent)
@@ -21,12 +23,14 @@ class App(tk.Frame):
         frame3=tk.Frame(master=parent)
         frame3.pack()
         self.fig,self.ax = plt.subplots(1,1)
+        self.line, = self.ax.plot(x,y)
         self.ax.xaxis_date()
         self.fig.autofmt_xdate(rotation=45)
         plt.gcf().autofmt_xdate()
         myfmt = mdates.DateFormatter('%Y-%m-%d %H:%M:%S')
         plt.gca().xaxis.set_major_formatter(myfmt)
         self.ax.autoscale(enable=True)
+        self.ax.grid()
         
         self.canvas = FigureCanvasTkAgg(self.fig, master=frame1)
         self.canvas.get_tk_widget().pack()
@@ -35,15 +39,15 @@ class App(tk.Frame):
         self.toolbar.update()
         self.canvas._tkcanvas.pack()
         self.canvas.mpl_connect("key_press_event", self.on_key_press)
-
+        self.canvas.draw()
+        
         lbutton = tk.Button(master=frame2, text="<<", command=lambda: self.update_plot('left'))
         lbutton.grid(column=0,row=0)
         cbutton = tk.Button(master=frame2, text="Refresh", command=lambda: self.update_plot('start'))
         cbutton.grid(column=1,row=0)
         rbutton = tk.Button(master=frame2, text=">>", command=lambda: self.update_plot('right'))
         rbutton.grid(column=2,row=0)
-        self.plot_chan=tk.StringVar()
-        self.plot_chan.set('1')
+
         for n in range(1,13):
             tk.Radiobutton(master=frame3,text=f'{n}',variable=self.plot_chan,value=str(n)).pack(side=tk.LEFT)
         self.update_plot('left')
@@ -51,7 +55,7 @@ class App(tk.Frame):
         if text=='left':
             self.plot_date = self.plot_date - datetime.timedelta(days=1)
         if text=='right':
-            self.plot_date = self.plot_date + datetime.timedelta(days=1)    
+            self.plot_date = self.plot_date + datetime.timedelta(days=1)
         channum = self.plot_chan.get()
         for chan in self.dic:
             if chan != channum:
@@ -61,19 +65,26 @@ class App(tk.Frame):
                 if self.plot_date.date() == t.date():
                     x.append(t)
                     y.append(z)
+            if len(x) == 0:
+                tk.messagebox.showinfo("Warning", f"No data is found for this day:\n   {self.plot_date.date()}")
+                if text=='left':
+                    self.plot_date = self.plot_date + datetime.timedelta(days=1)
+                if text=='right':
+                    self.plot_date = self.plot_date - datetime.timedelta(days=1)
+                return
             try:
-                self.toolbar.update()
-                self.ax.plot(x,y)
+                
+                self.line.set_data(x,y)
 
                 self.ax.set_title(f'Channel: {chan}, {self.plot_date.date()}')
                 
                 self.ax.relim()
                 self.ax.autoscale(enable=True)
                 self.canvas.draw()
+                self.toolbar.update()
             except Exception as e:
                 print(e)
                 return
-            del self.ax.lines[:10]
 
     def on_key_press(self, event):
         key_press_handler(event, self.canvas, self.toolbar)
@@ -94,8 +105,15 @@ class App(tk.Frame):
                 self.dic[chan][1].append(float(line[2]))
                 self.dic[chan][0].append(timestamp)
             self.plot_date = datetime.datetime.strptime(line[0].split()[0], "%m/%d/%Y")
-        
-##        plt.ion()
+        x,y=[],[]
+        for chan in self.dic:
+            if chan != self.plot_chan.get():
+                continue
+            for t,z in zip(self.dic[chan][0],self.dic[chan][1]):
+                if self.plot_date.date() == t.date():
+                    x.append(t)
+                    y.append(z)
+        return x,y
 
 if __name__ == '__main__':
     root = tk.Tk()
