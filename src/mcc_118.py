c084df9b10ef23a8e96ea933ee07d1fffdcf1b25
seamuss1 <seamusdgallagher@gmail.com>
2019-07-06 14:06:03 -0400

    Added compatibility for Cortix

--- src/mcc_118.py
+++ src/mcc_118.py
@@ -15,11 +15,12 @@ import os, sys, io, time, datetime, traceback
 import logging
 
 from daqhats import mcc118, OptionFlags, HatIDs, HatError, hat_list
+from cortix.src.module import Module
 #*********************************************************************************
 
-class MCC_118():
+class MCC_118(Module):
     r'''
-
+    Class for MCC_118 analog voltage data acquisition device
     '''
 
 #*********************************************************************************
@@ -27,31 +28,26 @@ class MCC_118():
 #*********************************************************************************
 
     def __init__( self, wrk_dir='/tmp/dados',db_dir='IR_7040_db'):
-        print('start')
-
+        print('MCC_118 class start')
         self.wrk_dir = wrk_dir
-        self.db_dir=db_dir
-        if not os.path.isdir(wrk_dir):
-            os.makedirs(wrk_dir)
+        home=os.path.expanduser('~')
+        self.db_dir=os.path.join(home,db_dir)
+        for d in [self.wrk_dir,self.db_dir]:
+            if not os.path.isdir(d):
+                os.makedirs(d)
 
     def run(self ,channels=[0,2,4]):
         '''
-
+        Run class that will be called by Cortix
+        Returns data in predetermined format (format undecided)
         '''
-        filename = self.wrk_dir+'/mcc_118_data.csv'
-##        with open(filename,'w') as f:
-##            f.write('')
         hatlist = hat_list()
-        print(hatlist)
         for i in hatlist:
             ad = i.address
         hat = mcc118(ad)
         options = OptionFlags.DEFAULT
-        home=os.path.expanduser('~')
-        directory=home+os.path.join(home,self.db_dir)
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
         avgs=dict()
+
         while True:
             for i in channels:
                 if str(i) not in avgs:
@@ -68,21 +64,9 @@ class MCC_118():
             print(avgs)
             avgs=dict()
             continue
-            mean=dict()
-            for i in channels:
-                mean=sum(avg)/len(avg)
-            avg=[]
-            print(mean,value2,value4)
-            timestamp=str(datetime.datetime.now())[:-7]
-            filename2='{}/mcc_118_data_{}'.format(directory,str(datetime.datetime.now())[:10])
-##            with open(filename,'w') as f:
-##                f.write(str(mean))
-            with open(filename2,'a') as file:
-                file.write('{}, {}\n'.format(str(mean),timestamp))
-##            if self.mccevent.isSet():
-##                return
 
 #======================= end class MCC118: =======================================
+
 if __name__=='__main__':
     app = MCC_118()
     app.run()
--- src/rs_232.py
+++ src/rs_232.py
@@ -12,12 +12,12 @@ IR 7040 "intelligent ratemeter from Mirion Tech. Inc.
 '''
 #*********************************************************************************
 import os, sys, io, time, datetime, traceback, threading
-import logging
+import logging, serial
 
-import serial
+from cortix.src.module import Module
 #*********************************************************************************
 
-class RS_232(threading.Thread):
+class RS_232(Module):
     r'''
     RS-232 class for Dados. Serial communication with various devices.
     '''
@@ -26,42 +26,13 @@ class RS_232(threading.Thread):
 # Construction 
 #*********************************************************************************
 
-    def __init__( self, wrk_dir='/tmp/dados',filename='ir_data',rsevent=None):
-        self.rsevent=rsevent
+    def __init__( self, wrk_dir='/tmp/dados',filename='ir_data'):
         self.filename=filename
         if not os.path.isdir(wrk_dir):
             os.makedirs(wrk_dir)
         self.__wrk_dir = wrk_dir
 
-#        if device_name == 'ir-7040':
-#        else:
-#            assert device_name == 'ir-7040','device name: %r'%device_name
-
-        return
-
-#*********************************************************************************
-# Public member functions
-#*********************************************************************************
-
-    def execute( self, cortix_time=0.0, cortix_time_step=0.0 ):
-        '''
-        Developer must implement this method.
-        Evolve system from cortix_time to cortix_time + cortix_time_step
-        '''
-
-        s = 'execute('+str(round(cortix_time,2))+'[min]): '
-        self.__log.debug(s)
-
-        # Developer implements helper method, for example
-        #self.__evolve( self, cortix_time, cortix_time_step ):
-
-        return
-
-#*********************************************************************************
-# Private helper functions (internal use: __)
-#*********************************************************************************
-
-    def ir_7040(self,timeID='',event=None):
+    def run(self,timeID=''):
         '''
         IR 7040 "intelligent ratemeter from Mirion Tech. Inc.
         '''
@@ -89,34 +60,16 @@ class RS_232(threading.Thread):
                 time.sleep(.25)
                 continue
             self.timestamp=str(datetime.datetime.now())[:-7]
-#            print(line)
-            try:
-                if self.rsevent.isSet():
-#                    print('Thread killed')
-                    return
-            except AttributeError:
-                pass
+            print(line)
             olddata=line
             splitline=line.split()
             splitline.append(self.timestamp)
             for n in range(2,8):
                 splitline[n] = splitline[n][0]+'.'+splitline[n][1:3]+'e'+splitline[n][3:]
-            filename='{}/ir_7040_data_{}.csv'.format(directory,str(datetime.datetime.now())[:10])
-            if not os.path.isfile(filename):
-                with open(filename,'a') as f:
-                    f.write('Type, Callback, ch1_rate_filtered, ch1_rate_unfiltered, ch1_dose, ch1_alarm_high, ch1_alarm_low\
-                            , ch2_rate_filtered, ch2_rate_unfiltered, ch2_dose, ch2_alarm_high, ch2_alarm_low\
-                            , Leak Rate: Gallons/Day, Leak Rate: %Power Level\
-                            , ch3_rate_filtered, ch3_rate_unfiltered, ch3_dose, ch3_alarm_high, ch3_alarm_low\
-                            , ch4_rate_filtered, ch4_rate_unfiltered, ch4_dose, ch4_alarm_high, ch4_alarm_low\
-                            , Checksum, Probe Status, Date and Time\n')
-            padding = ', '.join(splitline)
-            commaline=padding
-            padding += (" " * (100 - len(padding)))
-            c=0
-            with open(tempfile,'w') as file:
-                file.write(commaline)
-            with open(filename,'a') as f:
-                f.write('{}\n'.format(commaline))
 
-#======================= end class Dados: ========================================
+
+
+
+if __name__=='__main__':
+    app = RS_232()
+    app.run()
