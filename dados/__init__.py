bb12529f461435d504d92de2e3476a2082f1cdda
seamuss1 <seamusdgallagher@gmail.com>
2019-08-25 17:47:01 -0400

    Changes completing dados first example application

--- dados/__init__.py
+++ dados/__init__.py
@@ -3,8 +3,5 @@
 
 from .dados_main import Dados
 from .devices.IR_7040 import IR_7040
-try:
-    from .devices.mcc_118 import MCC_118
-except ModuleNotFoundError as e:
-    'DAQHats library not installed'
-    pass
+from .devices.mcc_118 import MCC_118
+from .visualization.plot_data import Dataplot
